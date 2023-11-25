1493a81e7e1ebae5898e2b3f98712516b2951dd8
Michael Maloney <michael@maloney.at>
2023-11-25 03:08:31 +0000

    Remove wayward unreachable!().

--- nettle/src/circuit.rs
+++ nettle/src/circuit.rs
@@ -254,8 +254,6 @@ impl Component {
                         _ => (),
                     }
                 }
-            } else {
-                unreachable!()
             }
         }
         errors
