9f88859d52b106f12148065ef5447cd8fb181f70
Michael Maloney <michael@maloney.at>
2023-12-08 07:27:18 +0000

    Split up large expr.rs and circuit.rs.

---
+++ nettle/src/expr/eval.rs
@@ -0,0 +1,201 @@
+use super::*;
+
+impl Expr {
+    pub fn eval(&self, nettle: &Sim) -> Value {
+        match self {
+            Expr::Reference(_loc, path) => nettle.peek(path.clone()),
+            Expr::Net(_loc, netid) => nettle.peek_net(*netid),
+            Expr::Lit(_loc, value) => value.clone(),
+            Expr::UnOp(_loc, op, e) => {
+                match (op, e.eval(nettle)) {
+                    (UnOp::Not, Value::Word(n, v)) => Value::Word(n, (!v) & ((1 << n) - 1)),
+                    _ => Value::X,
+                }
+            },
+            Expr::BinOp(_loc, op, e1, e2) => {
+                match (op, e1.eval(nettle), e2.eval(nettle)) {
+                    (BinOp::Add, Value::X, _other) => Value::X,
+                    (BinOp::Add, _other, Value::X) => Value::X,
+                    (BinOp::Add, Value::Word(w, a),  Value::Word(_w, b)) => Value::Word(w, a.wrapping_add(b) % (1 << w)),
+                    (BinOp::AddCarry, Value::Word(w, a),  Value::Word(_w, b)) => {
+                        let new_w = w + 1;
+                        Value::Word(new_w, a.wrapping_add(b) % (1 << new_w))
+                    },
+                    (BinOp::Sub, Value::Word(w, a),  Value::Word(_w, b)) => Value::Word(w, a.wrapping_sub(b) % (1 << w)),
+                    (BinOp::And, Value::Word(w, a),  Value::Word(_w, b)) => Value::Word(w, a & b),
+                    (BinOp::Or,  Value::Word(w, a),  Value::Word(_w, b)) => Value::Word(w, a | b),
+                    (BinOp::Eq,  Value::Word(_w, a), Value::Word(_v, b)) => (a == b).into(),
+                    (BinOp::Eq,  Value::Enum(_typedef, a), Value::Enum(_typedef2, b)) => (a == b).into(),
+                    (BinOp::Lt,  Value::Word(_w, a), Value::Word(_v, b)) => (a < b).into(),
+                    (BinOp::Neq, Value::Word(_w, a), Value::Word(_v, b)) => (a != b).into(),
+                    (BinOp::Xor, Value::Word(n, a),  Value::Word(_m, b)) => Value::Word(n, a ^ b),
+                    _ => Value::X,
+                }
+            },
+            Expr::If(_loc, cond, e1, e2) => {
+                let cond_v = cond.eval(nettle);
+                let v1 = e1.eval(nettle);
+                let v2 = e2.eval(nettle);
+                if cond_v.is_x() || v1.is_x() || v2.is_x() {
+                    return Value::X;
+                }
+                match cond_v {
+                    Value::Word(1, 1) => v1,
+                    Value::Word(1, 0) => v2,
+                    _ => Value::X,
+                }
+            },
+            Expr::Mux(_loc, cond, e1, e2) => {
+                let cond_v = cond.eval(nettle);
+                let v1 = e1.eval(nettle);
+                let v2 = e2.eval(nettle);
+                if cond_v.is_x() || v1.is_x() || v2.is_x() {
+                    return Value::X;
+                }
+                match cond_v {
+                    Value::Word(1, 1) => v1,
+                    Value::Word(1, 0) => v2,
+                    _ => Value::X,
+                }
+            },
+            Expr::Cat(_loc, es) => {
+                let mut cat_width: u64 = 0;
+                let mut cat_val: u64 = 0;
+                let mut wss: Vec<Value> = vec![];
+                for v in es.iter().map(|e| e.eval(nettle)).rev() {
+                    if let Value::X = v {
+                        return Value::X;
+                    } else if let Value::Word(width, val) = v {
+                        cat_val |= val << cat_width;
+                        cat_width += width;
+                    } else if let Value::Vec(ws) = v {
+                        wss.extend(ws.into_iter().rev());
+                    } else {
+                        panic!("Can't cat on a non-Word");
+                    }
+                }
+                if wss.len() == 0 {
+                    Value::Word(cat_width, cat_val)
+                } else {
+                    Value::Vec(wss.into_iter().rev().collect())
+                }
+            },
+            Expr::Sext(_loc, e, n) => {
+                match e.eval(nettle) {
+                    Value::X => Value::X,
+                    Value::Word(0, _x) => panic!("Can't sext a Word<0>"),
+                    Value::Word(w, x) => {
+                        if w <= *n {
+                            let is_negative = x & (1 << (w - 1)) > 0;
+                            if is_negative {
+                                let flips = ((1 << (n - w)) - 1) << w;
+                                Value::Word(*n, flips | x)
+                            } else {
+                                Value::Word(*n, x)
+                            }
+                        } else {
+                            panic!("Can't sext a Word<{w}> to Word<{n}> because {w} > {n}.")
+                        }
+                    },
+                    Value::Vec(_vs) => panic!("Can't sext a Vec"),
+                    Value::Enum(typedef, _name) => panic!("Can't sext a {}", typedef.name()),
+                }
+            }
+            Expr::ToWord(_loc, e) => {
+                let v = e.eval(nettle);
+                match v {
+                    Value::X => Value::X,
+                    Value::Enum(typedef, name) => typedef.get().unwrap().value_of(&name).unwrap(),
+                    _ => panic!("Can only call word() on enum values, but found {v:?}"),
+                }
+            },
+            Expr::Vec(_loc, es) => {
+                let mut vs = vec![];
+                for v in es.iter().map(|e| e.eval(nettle)) {
+                    if let Value::X = v {
+                        return Value::X;
+                    } else {
+                        vs.push(v.clone());
+                    }
+                }
+                Value::Vec(vs)
+            },
+            Expr::Idx(_loc, e, i) => {
+                let value = e.eval(nettle);
+                if let Value::X = value {
+                    Value::X
+                } else if let Value::Word(width, val) = value {
+                    if *i < width {
+                        Value::Word(1, (val >> i) & 1)
+                    } else {
+                        panic!("Index at {i} out of range (width {width})")
+                    }
+                } else if let Value::Vec(vs) = value {
+                    if *i < vs.len().try_into().unwrap() {
+                        vs[*i as usize].clone()
+                    } else {
+                        panic!("Index at {i} out of range (length {})", vs.len())
+                    }
+                } else {
+                        panic!("Index with invalid value: {value:?}")
+                }
+            },
+            Expr::IdxRange(_loc, e, j, i) => {
+                let value = e.eval(nettle);
+                if let Value::X = value {
+                    Value::X
+                } else if let Value::Word(width, val) = value {
+                    // TODO make errors better
+                    if width >= *j && *j >= *i {
+                        let new_width = j - i;
+                        // eg, if new_width = 3, shift over 3 to get 0b1000
+                        // then subtract 1 to get 0b01111
+                        let mask = (1 << new_width) - 1;
+                        Value::Word(new_width, (val >> i) & mask)
+                    } else {
+                        panic!("Index {j}..{i} out of range (width {width})")
+                    }
+                } else if let Value::Vec(vs) = value {
+                    let width = vs.len();
+                    if j <= i && *i <= width as u64 {
+                        Value::Vec(vs[*j as usize..*i as usize].to_vec())
+                    } else {
+                        panic!("Index {j}..{i} out of range (length {width})")
+                    }
+                } else {
+                    panic!("Can't index into value: {value:?}")
+                }
+            },
+            Expr::IdxDyn(_loc, e, i) => {
+                let index = if let Value::Word(_width, val) = i.eval(nettle) {
+                    val
+                } else {
+                    panic!("Invalid index: {i:?}");
+                };
+
+                let value = e.eval(nettle);
+                if let Value::Word(width, val) = value {
+                    if index < val {
+                        Value::Word(1, (val >> index) & 1)
+                    } else {
+                        panic!("Index at {index} out of range (width {width})")
+                    }
+                } else if let Value::Vec(vs) = value {
+                    if index < vs.len().try_into().unwrap() {
+                        vs[index as usize].clone()
+                    } else {
+                        panic!("Index at {index} out of range (length {})", vs.len())
+                    }
+                } else {
+                    panic!("Index with invalid value: {value:?}")
+                }
+            },
+            Expr::Hole(_loc, opt_name) => {
+                match opt_name {
+                    Some(name) => panic!("EVALUATED A HOLE: ?{name}"),
+                    None => panic!("EVALUATED A HOLE"),
+                }
+            },
+        }
+    }
+}
---
+++ nettle/src/expr/typecheck.rs
@@ -0,0 +1,221 @@
+use super::*;
+
+impl Expr {
+    #[allow(unused_variables)] // TODO remove this
+    pub fn typecheck(&self, type_expected: &Type, ctx: Context<Path, Type>) -> Result<(), TypeError> {
+        if let Some(type_actual) = self.typeinfer(ctx.clone()) {
+            if &type_actual == type_expected {
+                return Ok(());
+            } else {
+                return Err(TypeError::NotExpectedType(type_expected.clone(), type_actual.clone(), self.clone()));
+            }
+        }
+
+        match self {
+            Expr::Reference(_loc, path) => Err(TypeError::UndefinedReference(self.clone())),
+            Expr::Net(_loc, netid) => panic!("Can't typecheck a net"),
+            Expr::Lit(_loc, Value::Word(w, n)) if n >> w != 0 =>
+                Err(TypeError::InvalidLit(self.clone())),
+            Expr::Lit(_loc, _) => unreachable!(),
+            Expr::UnOp(_loc, _op, e) => e.typecheck(type_expected, ctx.clone()),
+            Expr::BinOp(_loc, _op, e1, e2) => {
+                e1.typecheck(type_expected, ctx.clone())?;
+                e2.typecheck(type_expected, ctx.clone())?;
+                Ok(())
+            },
+            Expr::If(_loc, cond, e1, e2) => {
+                cond.typecheck(&Type::Word(1), ctx.clone())?;
+                e1.typecheck(type_expected, ctx.clone())?;
+                e2.typecheck(type_expected, ctx.clone())?;
+                Ok(())
+            },
+            Expr::Mux(_loc, cond, e1, e2) => {
+                cond.typecheck(&Type::Word(1), ctx.clone())?;
+                e1.typecheck(type_expected, ctx.clone())?;
+                e2.typecheck(type_expected, ctx.clone())?;
+                Ok(())
+            },
+            Expr::Cat(_loc, _es) => unreachable!(),
+            Expr::Sext(_loc, e, n) => {
+                if let Some(Type::Word(m)) = e.typeinfer(ctx.clone()) {
+                    if *n >= m {
+                        Ok(())
+                    } else {
+                        Err(TypeError::Other(self.clone(), format!(" Can't sext a Word<{m}> to a a Word<{n}>")))
+                    }
+                } else {
+                    Err(TypeError::CantInferType(self.clone()))
+                }
+            },
+            Expr::ToWord(_loc, e) => todo!(),
+            Expr::Vec(_loc, es) => {
+                if let Type::Vec(typ, n) = type_expected {
+                    for e in es {
+                        e.typecheck(typ, ctx.clone())?;
+                    }
+                    if es.len() != *n as usize {
+                        let type_actual = Type::Vec(typ.clone(), es.len().try_into().unwrap());
+                        Err(TypeError::NotExpectedType(type_expected.clone(), type_actual.clone(), self.clone()))
+                    } else {
+                        Ok(())
+                    }
+                } else {
+                    Err(TypeError::Other(self.clone(), format!("Expected {type_expected:?} but found a Vec.")))
+                }
+            },
+            Expr::Idx(_loc, e, i) => {
+                match e.typeinfer(ctx.clone()) {
+                    Some(Type::Word(n)) if *i < n => Ok(()),
+                    Some(Type::Word(n)) => Err(TypeError::Other(self.clone(), format!("Index out of bounds"))),
+                    Some(typ) => Err(TypeError::Other(self.clone(), format!("Can't index into type {typ:?}"))),
+                    None => Err(TypeError::Other(self.clone(), format!("Can't infer the type of {e:?}"))),
+                }
+            },
+            Expr::IdxRange(_loc, e, j, i) => {
+                match e.typeinfer(ctx.clone()) {
+                    Some(Type::Word(n)) if n >= *j && j >= i => Ok(()),
+                    Some(Type::Word(_n)) => Err(TypeError::Other(self.clone(), format!("Index out of bounds"))),
+                    Some(typ) => Err(TypeError::Other(self.clone(), format!("Can't index into type {typ:?}"))),
+                    None => Err(TypeError::Other(self.clone(), format!("Can't infer the type of {e:?}"))),
+                }
+            },
+            Expr::IdxDyn(_loc, e, i) => todo!(),
+            Expr::Hole(_loc, opt_name) => Ok(()),
+        }
+    }
+
+    #[allow(unused_variables)] // TODO remove this
+    pub fn typeinfer(&self, ctx: Context<Path, Type>) -> Option<Type> {
+        match self {
+            Expr::Reference(_loc, path) => ctx.lookup(path),
+            Expr::Net(_loc, netid) => panic!("Can't typecheck a net"),
+            Expr::Lit(_loc, value) => match value {
+                Value::Word(w, n) if n >> w == 0 => Some(Type::Word(*w)),
+                Value::Word(_w, _n) => None,
+                Value::Vec(_es) => None,
+                Value::Enum(typedef, _name) => Some(Type::TypeDef(typedef.clone())),
+                Value::X => None,
+            },
+            Expr::UnOp(_loc, _op, e) => e.typeinfer(ctx.clone()),
+            Expr::BinOp(_loc, BinOp::AddCarry, e1, e2) => {
+                let typ1 = e1.typeinfer(ctx.clone())?;
+                let typ2 = e2.typeinfer(ctx.clone())?;
+                if typ1 == typ2 {
+                    match typ1 {
+                        Type::Word(w) => Some(Type::Word(w + 1)),
+                        _ => None,
+                    }
+                } else {
+                    None
+                }
+            },
+            Expr::BinOp(_loc, op, e1, e2) => {
+                let typ1 = e1.typeinfer(ctx.clone())?;
+                let typ2 = e2.typeinfer(ctx.clone())?;
+                if typ1 == typ2 {
+                    if *op == BinOp::Eq || *op == BinOp::Neq || *op == BinOp::Lt {
+                        Some(Type::Word(1))
+                    } else {
+                        Some(typ1)
+                    }
+                } else {
+                    None
+                }
+            },
+            Expr::If(_loc, cond, e1, e2) => {
+                cond.typecheck(&Type::Word(1), ctx.clone()).ok()?;
+                let typ1 = e1.typeinfer(ctx.clone())?;
+                let typ2 = e2.typeinfer(ctx.clone())?;
+                if typ1 == typ2 {
+                    Some(typ1)
+                } else {
+                    None
+                }
+            },
+            Expr::Mux(_loc, cond, e1, e2) => {
+                cond.typecheck(&Type::Word(1), ctx.clone()).ok()?;
+                let typ1 = e1.typeinfer(ctx.clone())?;
+                let typ2 = e2.typeinfer(ctx.clone())?;
+                if typ1 == typ2 {
+                    Some(typ1)
+                } else {
+                    None
+                }
+            },
+            Expr::Cat(_loc, es) => {
+                let mut w = 0u64;
+                for e in es {
+                    if let Some(Type::Word(m)) = e.typeinfer(ctx.clone()) {
+                        w += m;
+                    } else {
+                        return None;
+                    }
+                }
+                Some(Type::Word(w))
+            },
+            Expr::Sext(_loc, e, n) => None,
+            Expr::ToWord(loc, e) => {
+                match e.typeinfer(ctx.clone()) {
+                    Some(Type::TypeDef(typedef)) => {
+                        if let Some(typedef) = typedef.get() {
+                            Some(Type::Word(typedef.width()))
+                        } else {
+                            panic!("Unresolved typedef: {:?} location: {loc:?}", typedef.name())
+                        }
+                    }
+                    _ => None,
+                }
+            },
+            Expr::Vec(_loc, es) => None,
+            Expr::Idx(_loc, e, i) => {
+                match e.typeinfer(ctx.clone()) {
+                    Some(Type::Word(n)) if *i < n => Some(Type::Word(1)),
+                    _ => None,
+                }
+            },
+            Expr::IdxRange(_loc, e, j, i) => {
+                match e.typeinfer(ctx.clone()) {
+                    Some(Type::Word(n)) if n >= *j && j >= i => Some(Type::Word(j - i)),
+                    Some(Type::Word(n)) => None,
+                    Some(typ) => None,
+                    None => None,
+                }
+            },
+            Expr::IdxDyn(_loc, e, i) => None,
+            Expr::Hole(_loc, opt_name) => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum TypeError {
+    UndefinedReference(Expr),
+    NotExpectedType(Type, Type, Expr),
+    InvalidLit(Expr),
+    CantInferType(Expr),
+    Other(Expr, String),
+}
+
+impl HasLoc for TypeError {
+    fn loc(&self) -> Loc {
+        match self {
+            TypeError::UndefinedReference(e) => e.loc(),
+            TypeError::NotExpectedType(_type_expected, _type_actual , e) => e.loc(),
+            TypeError::InvalidLit(e) => e.loc(),
+            TypeError::CantInferType(e) => e.loc(),
+            TypeError::Other(e, _msg) => e.loc(),
+        }
+    }
+}
+
+impl std::fmt::Display for TypeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            TypeError::UndefinedReference(expr) => write!(f, "Undefiend reference: {expr:?}"),
+            TypeError::NotExpectedType(type_expected, type_actual, expr) => write!(f, "Not expected type: {expr:?} has type {type_actual:?} but expected {type_expected:?}."),
+            TypeError::InvalidLit(expr) => write!(f, "Invalid literal: {expr:?}"),
+            TypeError::CantInferType(expr) => write!(f, "Can't infer type: {expr:?}"),
+            TypeError::Other(_expr, _string) => write!(f, "{self:?}"),
+        }
+    }
+}
