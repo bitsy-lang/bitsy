b90b8984e0721d505b568c8f6ca431e7feaa69cf
Michael Maloney <michael@maloney.at>
2024-01-01 13:23:01 -0700

    Get JAL/JALR working.

--- riscv-bitsy/programs/hello.s
+++ riscv-bitsy/programs/hello.s
@@ -2,18 +2,23 @@
     .globl _start
 
 _start:
-    li gp, 0x10000
-    li t0, 0x0
+    li gp, 0x10000  # 0
+    li sp, 0x100    # 4
+    li t0, 0x0      # 8
 
 .loop:
-    lw t1, 0(t0)
-    andi t1, t1, 0xff
-    beqz t1, .halt
-    sw t1, 0(gp)
-    addi t0, t0, 1
-
-    j .loop
+    lw t1, 0(t0)    # 12
+    beqz t1, .halt  # 16
+    mv a0, t1       # 20
+    call putc       # 24
+    addi t0, t0, 1  # 28
+    j .loop         # 32
 
 .halt:
-    ebreak
-    j .halt
+    ebreak          # 36
+    j .halt         # 40
+
+putc:
+    andi a0, a0, 0xff   # 44
+    sw a0, 0(gp)        # 48
+    jalr zero, 0(ra)
